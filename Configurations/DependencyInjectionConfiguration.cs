2fae46f2d910ed11f7857d74f78b6da0d81f942f
harisson.duraes <harisson.duraes@ATA-TI58>
2024-05-09 17:28:17 -0300

    Cria modelagem UML

--- Configurations/DependencyInjectionConfiguration.cs
+++ Configurations/DependencyInjectionConfiguration.cs
@@ -6,7 +6,6 @@ namespace API.Configurations
     {
         public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
         {
-            //services.AddScoped<Contexts.MongoDBContext>();
             services.AddScoped<Repositories.Interfaces.ICadPessoaRepository, Repositories.Implementations.CadPessoaRepository>();
             return services;
         }
--- obj/Debug/net6.0/API.AssemblyInfo.cs
+++ obj/Debug/net6.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90777a1f0be8148c37305e983e7ea7def72670eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b1104ff39133d45ae949a7cd73fd4f093b900e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
