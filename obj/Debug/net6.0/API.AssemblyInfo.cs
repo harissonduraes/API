acd283c6e647edee4c89c3cba4f8a966eefdb829
harisson.duraes <harisson.duraes@ATA-TI58>
2024-05-10 08:52:34 -0300

    Adiciona imagem de modelagem de classe

--- obj/Debug/net6.0/API.AssemblyInfo.cs
+++ obj/Debug/net6.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b1104ff39133d45ae949a7cd73fd4f093b900e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fae46f2d910ed11f7857d74f78b6da0d81f942f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
